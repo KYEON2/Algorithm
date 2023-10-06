182630aae4bb755fdcdff6616ded1f9b103d83b7
Kiyeon Roh <107321721+KYEON2@users.noreply.github.com>
2023-10-06 22:43:47 +0900

    [Silver V] Title: 색종이, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int area[100][100]= {0, };
+
+    int n, r, h;
+    int cnt = 0;
+    scanf("%d", &n);
+    for(int i=0; i<n; i++){
+        scanf("%d %d", &r, &h);
+        for(int m=100-(h+10); m<100-h; m++){
+            for(int k=r; k<r+10; k++){
+                area[m][k]=1;
+            }
+        }
+    }
+    for(int i=0; i<100; i++){
+        for(int j=0; j<100; j++){
+            if(area[i][j]==1){
+                cnt ++;
+            }
+        }
+    }
+    printf("%d", cnt);
+    return 0;
+}
